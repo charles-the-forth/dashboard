fac4754ea650e569aa64f3ba2ebc8f70c0f4c40f
Václav Pavlíček <venda.pavlicek@gmail.com>
2019-04-22 11:16:14 +0200

    Update favicon.

--- client/src/components/CanSatAppBar/CanSatAppBar.js
+++ client/src/components/CanSatAppBar/CanSatAppBar.js
@@ -3,7 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import logo from '../../images/cansat.png';
 import { withStyles } from '@material-ui/core/styles';
-import SignalProgress from '../SignalProgress/SignalProgress';
 import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
@@ -14,18 +13,6 @@ const styles = theme => ({
     logo: {
         width: '36px',
         paddingRight: '8px'
-    },
-    grow: {
-        display: 'flex',
-        flexGrow: '1',
-        justifyContent: 'center'
-    },
-    signalContainer: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-        marginTop: '10px',
-        width: '212px',
-        opacity: 0
     }
 });
 
@@ -41,12 +28,6 @@ const CanSatAppBar = ({ classes, signal, children }) => {
                     noWrap>
                     Charles the Fourth
                 </Typography>
-                <div className={classes.grow}>
-                    {children}
-                </div>
-                <div className={classes.signalContainer}>
-                    <SignalProgress signal={signal}/>
-                </div>
             </Toolbar>
         </AppBar>
     );
