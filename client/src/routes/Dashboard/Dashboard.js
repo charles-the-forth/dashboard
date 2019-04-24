b0022250c9e43c9380309d7c924ae95e8aebc068
Václav Pavlíček <venda.pavlicek@gmail.com>
2019-04-24 22:45:12 +0200

    Add waiting screen.

--- client/src/routes/Dashboard/Dashboard.js
+++ client/src/routes/Dashboard/Dashboard.js
@@ -15,6 +15,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import * as firebase from "firebase/app";
 import "firebase/firestore";
+import logo from '../../images/cansat.png';
 
 const styles = theme => ({
   logo: {
@@ -45,6 +46,18 @@ const styles = theme => ({
   },
   altitudePaper: {
     marginTop: '16px'
+  },
+  loadingContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    height: '100vh',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  loadingLogo: {
+    height: '256px',
+    width: '256px',
+    marginBottom: '32px'
   }
 });
 
@@ -53,6 +66,7 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      ready: false,
       temperature: [],
       pressure: [],
       humidity: [],
@@ -93,64 +107,65 @@ class Dashboard extends Component {
       .orderBy("messageId", "desc")
       .limit(25)
       .onSnapshot(querySnapshot => {
-           console.log('update');
+        const temperature = [];
+        const pressure = [];
+        const humidity = [];
+        const lightIntensity = [];
+        const altitude = [];
+        let messageId, year, month, day, hour, second, minute, lat, lng, numberOfSatellites;
+        let first = true;
+        let ready = false;
 
-          const temperature = [];
-          const pressure = [];
-          const humidity = [];
-          const lightIntensity = [];
-          const altitude = [];
-          let messageId, year, month, day, hour, second, minute, lat, lng, numberOfSatellites;
-          let first = true;
-
-          querySnapshot.forEach(doc => {
-              const data = doc.data();
-              if (first) {
-                messageId = data.messageId;
-                year = data.year;
-                month = data.month;
-                day = data.day;
-                hour = data.hour;
-                second = data.second;
-                minute = data.minute;
-                lat = data.lat;
-                lng = data.lng;
-                numberOfSatellites = data.numberOfSatellites;
-              }
-              
-              temperature.push(data.temperature);
-              pressure.push(data.pressure);
-              humidity.push(data.humidity);
-              lightIntensity.push(data.lightIntensity);
-              altitude.push(data.altitude);
-          });
-
-          const center = {};
-          if (lat !== 0 && lng !== 0) {
-            center.lat = lat;
-            center.lng = lng;
-          } else {
-            center.lat = this.state.lat;
-            center.lng = this.state.lng;
+        querySnapshot.forEach(doc => {
+          const data = doc.data();
+          console.log(data);
+          if (first) {
+            messageId = data.messageId;
+            year = data.year;
+            month = data.month;
+            day = data.day;
+            hour = data.hour;
+            second = data.second;
+            minute = data.minute;
+            lat = data.lat;
+            lng = data.lng;
+            numberOfSatellites = data.numberOfSatellites;
+            ready = true;
           }
 
-          if (year === 2000 && month === 0 && day === 0) {
-            year = this.state.year;
-            month = this.state.month;
-            day = this.state.day;
-          }
-          
-          if (hour === 0 && minute === 0 && second === 0) {
-            hour = this.state.hour;
-            minute = this.state.minute;
-            second = this.state.second;
-          }
+          temperature.push(data.temperature);
+          pressure.push(data.pressure);
+          humidity.push(data.humidity);
+          lightIntensity.push(data.lightIntensity);
+          altitude.push(data.altitude);
+        });
 
-          this.setState({
-            temperature: reverse(temperature), pressure: reverse(pressure), humidity: reverse(humidity),
-            lightIntensity: reverse(lightIntensity), altitude: reverse(altitude), messageId, year, month, day, hour, second, minute, numberOfSatellites,
-            center
-          });
+        const center = {};
+        if (lat !== 0 && lng !== 0) {
+          center.lat = lat;
+          center.lng = lng;
+        } else {
+          center.lat = this.state.lat;
+          center.lng = this.state.lng;
+        }
+
+        if (year === 2000 && month === 0 && day === 0) {
+          year = this.state.year;
+          month = this.state.month;
+          day = this.state.day;
+        }
+
+        if (hour === 0 && minute === 0 && second === 0) {
+          hour = this.state.hour;
+          minute = this.state.minute;
+          second = this.state.second;
+        }
+
+        this.setState({
+          temperature: reverse(temperature), pressure: reverse(pressure), humidity: reverse(humidity),
+          lightIntensity: reverse(lightIntensity), altitude: reverse(altitude), messageId, year, month, day, hour, second, minute, numberOfSatellites,
+          center, ready: this.state.ready ? true : ready
+        });
       });
   }
 
@@ -174,63 +189,78 @@ class Dashboard extends Component {
   }
 
   render() {
-    const { classes, location } = this.props;
-    return (
-      <div>
-        <CanSatAppBar signal={80}>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap>
-            {pathOr('', ['state', 'measurement', 'name'], location)}
-          </Typography>
-        </CanSatAppBar>
-        <Grid container spacing={16} className={classes.mainGrid}>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Paper className={classes.paper}>
-              <Video config={this.state.config.video} />
-            </Paper>
+    const { classes } = this.props;
+
+    if (this.state.ready) {
+      return (
+        <div>
+          <CanSatAppBar signal={80}>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap>
+            </Typography>
+          </CanSatAppBar>
+          <Grid container spacing={16} className={classes.mainGrid}>
+            <Grid item xs={12} sm={6} lg={4}>
+              <Paper className={classes.paper}>
+                <Video config={this.state.config.video} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={4}>
+              <Paper className={classes.paper}>
+                <MapTile center={this.state.center} config={this.state.config.map} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={4}>
+              <Paper className={classes.paper}>
+                <TemperatureChart
+                  data={this.state.temperature}
+                  config={this.state.config.temperature} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={3}>
+              <Paper className={classes.paper}>
+                <PressureChart data={this.state.pressure} config={this.state.config.pressure} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={3}>
+              <Paper className={classes.paper}>
+                <HumidityChart data={this.state.humidity} config={this.state.config.humidity} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={3}>
+              <Paper className={classes.paper}>
+                <LightIntensityChart data={this.state.lightIntensity} config={this.state.config.lightIntensity} />
+              </Paper>
+              <Paper className={`${classes.paper} ${classes.altitudePaper}`}>
+                <AltitudeChart data={this.state.altitude} config={this.state.config.altitude} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={3} className={classes.infoTileGrid}>
+              <InfoTile icon={'message'} title='Message ID' text={this.state.messageId} />
+              <InfoTile icon={'satellite'} title='Number of satellites' text={this.state.numberOfSatellites} />
+              <InfoTile icon={'calendar_today'} title='Date' text={formatDate(this.state.day, this.state.month, this.state.year)} />
+              <InfoTile icon={'access_time'} title='Time' text={formatTime(this.state.hour, this.state.minute, this.state.second)} />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Paper className={classes.paper}>
-              <MapTile center={this.state.center} config={this.state.config.map} />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Paper className={classes.paper}>
-              <TemperatureChart
-                data={this.state.temperature}
-                config={this.state.config.temperature} />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper}>
-              <PressureChart data={this.state.pressure} config={this.state.config.pressure} />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper}>
-              <HumidityChart data={this.state.humidity} config={this.state.config.humidity} />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper}>
-              <LightIntensityChart data={this.state.lightIntensity} config={this.state.config.lightIntensity} />
-            </Paper>
-            <Paper className={`${classes.paper} ${classes.altitudePaper}`}>
-              <AltitudeChart data={this.state.altitude} config={this.state.config.altitude} />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3} className={classes.infoTileGrid}>
-            <InfoTile icon={'message'} title='Message ID' text={this.state.messageId} />
-            <InfoTile icon={'satellite'} title='Number of satellites' text={this.state.numberOfSatellites} />
-            <InfoTile icon={'calendar_today'} title='Date' text={formatDate(this.state.day, this.state.month, this.state.year)} />
-            <InfoTile icon={'access_time'} title='Time' text={formatTime(this.state.hour, this.state.minute, this.state.second)} />
-          </Grid>
-        </Grid>
-      </div >
-    );
+        </div >
+      );
+    } else {
+      return (
+        <div className={classes.loadingContainer}>
+            <img src={logo} className={classes.loadingLogo} alt="logo" />
+            <Typography
+              component="h1"
+              variant="h2"
+              color="inherit"
+              noWrap>
+              Chvilku strpení.
+            </Typography>
+        </div>        
+      );
+    }
   }
 }
 
@@ -244,7 +274,7 @@ const formatDate = (day, month, year) => {
 
 const formatTime = (hour, minute, second) => {
   if (hour !== undefined && minute !== undefined && second !== undefined) {
-    return ((hour < 10) ? '0' + hour.toString() : hour) + ':' + ((minute < 10) ? ('0' + minute.toString()): minute) + ':' + ((second < 10) ? ('0' + second.toString()): second);
+    return ((hour < 10) ? '0' + hour.toString() : hour) + ':' + ((minute < 10) ? ('0' + minute.toString()) : minute) + ':' + ((second < 10) ? ('0' + second.toString()) : second);
   }
   return '';
 };
